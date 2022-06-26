8a91f525504003cc6fcbe16d8f920e01ecde63d1
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2022-06-27 01:21:26 +0200

    Add files via upload

---
+++ autoencoders.py
@@ -0,0 +1,79 @@
+from torch import nn
+from torch.nn import functional as F
+
+
+# Shallow autoencoder architecture
+
+class AEobj(nn.Module):
+    def __init__(self, hidden_dim=None, input_dim=None):
+        super(AEobj, self).__init__()
+
+        # ensure an undercomplete AE
+        assert hidden_dim < input_dim
+        assert hidden_dim > 0
+        assert input_dim > 0
+
+        self.encoder = encoder(input_dim, hidden_dim)
+        self.decoder = decoder(input_dim, hidden_dim)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+class deepAEobj(nn.Module):
+    def __init__(self, hidden_dims=None, input_dim=None):
+        super(deepAEobj, self).__init__()
+
+        # encoders
+        self.encoder1 = encoder(input_dim, hidden_dims[0])
+        self.encoder2 = encoder(hidden_dims[0], hidden_dims[1])
+        self.encoder3 = encoder(hidden_dims[1], hidden_dims[2])
+
+        # decoders
+        self.decoder1 = decoder(input_dim, hidden_dims[0])
+        self.decoder2 = decoder(hidden_dims[0], hidden_dims[1])
+        self.decoder3 = decoder(hidden_dims[1], hidden_dims[2])
+
+    def forward(self, x):
+        # encode
+        x = self.encoder1(x)
+        x = self.encoder2(x)
+        x = self.encoder3(x)
+
+        # decode
+        x = self.decoder3(x)
+        x = self.decoder2(x)
+        x = self.decoder1(x)
+        return x
+
+class convAEobj(nn.Module):
+    def __init__(self, input_dim=None):
+        super(convAEobj, self).__init__()
+        
+
+
+#region encoder function g
+class encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super(encoder, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return x
+#endregion
+
+
+#region decoder function f
+class decoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super(decoder, self).__init__()
+        self.fc1 = nn.Linear(hidden_dim, input_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return x
+#endregion
+
+
---
+++ performance_Fashion_MNIST_using_deepAE.py
@@ -0,0 +1,171 @@
+import idx2numpy
+import numpy as np
+import torch
+
+from autoencoders import deepAEobj
+
+from torch import nn, optim
+from torch.utils.data import DataLoader, TensorDataset
+
+#region change directory [ignored it]
+import os
+print(os.getcwd())
+os.chdir(r'C:\Users\user\Documents\research-stat-summary\autoencoders')
+print(os.getcwd())
+
+#endregion
+
+
+## Fashion MNIST
+
+
+#region load images into tensor
+X_tr = idx2numpy.convert_from_file(r'train-images.idx3-ubyte')
+X_tst = idx2numpy.convert_from_file(r't10k-images.idx3-ubyte')
+
+(N_tr, nx, ny) = X_tr.shape
+(N_tst, _, _) = X_tst.shape
+
+# [avoid] UserWarning: The given NumPy array is not writeable,
+# and PyTorch does not support non-writeable tensors
+X_tr = np.array(X_tr.reshape(N_tr, nx*ny))
+X_tst = np.array(X_tst.reshape(N_tst, nx*ny))
+
+# normalize into [0, 1]
+X_tr = torch.Tensor(X_tr) / 255.0
+X_tst = torch.Tensor(X_tst) / 255.0
+
+dataset_tr, dataset_tst = TensorDataset(X_tr), TensorDataset(X_tst)
+
+batch_size = 64
+dataloader_tr = DataLoader(X_tr, batch_size=batch_size)
+dataloader_tst = DataLoader(X_tst, batch_size=batch_size)
+#endregion
+
+
+#region device, model and optimizer
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+hidden_dims = [392, 196, 98]
+model = deepAEobj(hidden_dims=hidden_dims, input_dim=nx*ny).to(device)
+print(model)
+
+criterion = nn.MSELoss(reduction='sum')
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+#endregion
+
+
+#region training
+max_epoch = 120
+loss_values = []
+loss_values_tst = []
+for epoch in range(max_epoch):
+
+    running_loss = []
+    model.train()
+    for i, x in enumerate(dataloader_tr, 0):
+        # sets gradients of all model parameters to zero.
+        optimizer.zero_grad()
+
+        # forward pass, backward and parameters update
+        x_pred = model(x.to(device))
+        loss = criterion(x_pred, x.to(device))
+        loss.backward()
+        optimizer.step()
+
+        # print loss
+        running_loss.append(loss.item()) # extract loss' value as a python float
+        if (i % 100 == 0) and (i > 0):
+            print(f'Epoch: {epoch} \t Iteration: {i} \t Loss: {np.round(np.mean(running_loss), decimals=2)}')
+
+    loss_values.append(np.mean(running_loss))
+
+    running_loss = []
+    model.eval()
+    for i, x in enumerate(dataloader_tst, 0):
+        x_pred = model(x.to(device))
+        loss = criterion(x_pred, x.to(device))
+        running_loss.append(loss.item())
+
+    loss_values_tst.append(np.mean(running_loss))
+#endregion
+
+
+#region save model
+out_path = r'C:\Users\user\Documents\research-stat-summary\autoencoders'
+model_ver = r'\deepAE_model1.pth' # .pt or .pth means a pytorch object
+torch.save(model.state_dict(), out_path + model_ver)
+
+#endregion
+
+
+#region plot loss
+from matplotlib import pyplot as plt
+
+plt.plot(np.array(loss_values), label='training loss')
+plt.plot(np.array(loss_values_tst), label='test loss')
+
+plt.xlabel('Epoch number')
+plt.ylabel('Loss (MSE)')
+plt.title('Loss curve')
+plt.legend(loc='upper right')
+#endregion
+
+
+#region selected example
+fig, axs = plt.subplots(1, 2)
+
+# source image
+idx_select = 90
+img_src = X_tr[idx_select, :].reshape((nx, ny)) # source image, reshape to 1D
+
+axs[0].imshow(img_src, cmap='gray', vmin=0, vmax=1)
+axs[0].set_title('Source image')
+axs[0].axis('off')
+
+# reconstructed image
+img_recon = torch.Tensor(img_src.reshape((1, nx*ny))) # reconstructed image
+img_recon = model(img_recon.to(device)) # prediction
+img_recon = img_recon.detach().cpu().numpy() # detach it from computation graph, move from gpu to cpu and convert to numpy
+img_recon = img_recon.reshape((nx, ny)) # reshape to 2D
+
+axs[1].imshow(img_recon, cmap='gray', vmin=0, vmax=1)
+axs[1].set_title('Reconstruction')
+axs[1].axis('off')
+
+#endregion
+
+
+#region latent space
+# load model
+device = torch.device('cpu')
+hidden_dims = [392, 196, 98]
+model = deepAEobj(hidden_dims=hidden_dims, input_dim=nx*ny)
+model.load_state_dict(torch.load('deepAE_model1.pth', map_location=device))
+
+# compute latent space
+latent_space = model.encoder3(model.encoder2(model.encoder1(X_tr.to(device))))
+latent_space = latent_space.detach().cpu().numpy()
+
+# latent space traversal
+col_select = 2
+h_min, h_max = np.min(latent_space[:, col_select]), np.max(latent_space[:, col_select])
+img_latent = latent_space[50, :]
+
+num_plots = 10
+fig, axs = plt.subplots(1, num_plots)
+for i, h in enumerate(np.linspace(h_min, h_max, num=num_plots)):
+    img_latent[col_select] = h
+    img_recon = model.decoder1(model.decoder2(model.decoder3(torch.Tensor(img_latent))))
+    img_recon = img_recon.reshape((nx, ny))
+
+    axs[i].imshow(img_recon.detach().numpy(), cmap='gray', vmin=0, vmax=1)
+    axs[i].set_title('Reconstruction')
+    axs[i].axis('off')
+
+# latent space correlation map
+import pandas as pd
+df = pd.DataFrame(latent_space)
+plt.imshow(df.corr())
